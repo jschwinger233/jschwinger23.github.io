1987ae7f1b009c67e0055b1c0c2e00caabf5a84c
zc <greyschwinger@gmail.com>
2020-03-02 23:53:36 +0800

    update send script

--- gists/send_tgch.py
+++ gists/send_tgch.py
@@ -3,40 +3,48 @@
 import os
 import sys
 import requests
+import itertools
+
+try:
+    chat_id = sys.argv[2]
+except IndexError:
+    chat_id = '@TheInfiniteSadness'
 
 
 def send(buf):
     text = ''.join(buf)
     print(text)
     payload = {
-        "chat_id": '@TheInfiniteSadness',
+        "chat_id": chat_id,
         "parse_mode": "Markdown",
         "text": text,
     }
     token = os.getenv("TGTOKEN")
     resp = requests.post(
-        f"https://api.telegram.org/bot{token}/sendMessage",
-        json=payload
+        f"https://api.telegram.org/bot{token}/sendMessage", json=payload
     )
     print(resp.json())
     resp.raise_for_status()
 
 
+id = os.path.basename(os.getcwd())
+filename = sys.argv[1].split('.')[0]
+declaim = f'\nfrom https://gist.github.com/jschwinger23/{id}#file-{filename}-md'
+
 buf = []
 length = 0
 code_block = False
 with open(sys.argv[1]) as f:
-    for line in f:
+    for line in itertools.chain(f, [declaim]):
         if line.startswith('```'):
             code_block = 1 - code_block
             if code_block:
                 code_block_buf = []
             else:
                 next_chunk = code_block_buf
-            code_block_buf.append('```')
+            code_block_buf.append('```\n')
 
         elif code_block:
-            next_chunk = []
             code_block_buf.append(line)
 
         elif not code_block:
@@ -45,13 +53,10 @@ with open(sys.argv[1]) as f:
         if length + sum(len(s) for s in next_chunk) > 4096:
             send(buf)
             buf = []
+            length = 0
 
-        else:
-            length += sum(len(s) for s in next_chunk)
-            buf.extend(next_chunk)
+        length += sum(len(s) for s in next_chunk)
+        buf.extend(next_chunk)
+        next_chunk = []
 
 send(buf)
-
-id = os.path.basename(os.getcwd())
-filename = os.argv[1].split('.')[0]
-send(f'from https://gist.github.com/jschwinger23/{id}#file-{filename}-md')
